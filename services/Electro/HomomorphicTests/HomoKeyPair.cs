62c49bec6af2d88b36bb99c376f45dcb3df0e6a7
Konstantin Plotnikov <kost@hackerdom.ru>
2015-11-05 04:10:20 +0500

    Electro service init

---
+++ services/Electro/HomomorphicTests/HomoKeyPair.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomomorphicTests
+{
+	class HomoKeyPair
+	{
+		public PublicKey publicKey;
+		public PrivateKey privateKey;
+		public int MaxNum;
+
+		public static HomoKeyPair GenKeyPair(int maxNum)
+		{
+			var privateKey = PrivateKey.GenPrivateKey(maxNum);
+			return new HomoKeyPair
+			{
+				MaxNum = maxNum,
+				privateKey = privateKey,
+				publicKey = PublicKey.GenPublicKey(privateKey, maxNum)
+			};
+		}
+	}
+
+	class PublicKey
+	{
+		public BigInteger[] PK;
+
+		public static PublicKey GenPublicKey(PrivateKey privateKey, int maxNum, int bitsCount = DefaultBitsCount)
+		{
+			var buff = new BigInteger[DefaultSetSize];
+
+			byte[] rand = new byte[bitsCount / 8];
+			Random r = new Random();
+
+			BigInteger rem;
+			for(int i = 0; i < buff.Length; i++)
+			{
+				r.NextBytes(rand);
+				buff[i] = (BigInteger.Abs(new BigInteger(rand)) * privateKey.P) + (maxNum * r.Next(10, 100));
+			}
+
+			return new PublicKey { PK = buff };
+		}
+
+		public const int DefaultBitsCount = 8 * 16;
+
+		public const int DefaultSetSize = 16;
+	}
+
+	class PrivateKey
+	{
+		public BigInteger P;
+
+		public static PrivateKey GenPrivateKey(int maxNum, int bitsCount = DefaultBitsCount)
+		{
+			var buff = new byte[bitsCount/8];
+			while(true)
+			{
+				new Random().NextBytes(buff);
+				var p = new BigInteger(buff);
+				if(p % maxNum == 0)
+					continue;
+
+				return new PrivateKey { P = p };
+			}
+		}
+
+		public const int DefaultBitsCount = 8 * 16;
+	}
+}
---
+++ services/Electro/HomomorphicTests/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomomorphicTests
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int maxNum = 67;
+			var nums = new []{4, 8, 15, 16, 23};
+
+			int testResult = 0;
+			BigInteger encResult = 0;
+
+			var homoKeyPair = HomoKeyPair.GenKeyPair(maxNum);
+
+			foreach(var num in nums)
+			{
+				testResult += num;
+				encResult += Encrypt(num, homoKeyPair.publicKey);
+			}
+
+			var decryptedResult = Decrypt(encResult, homoKeyPair.privateKey, maxNum);
+			Console.WriteLine(testResult == decryptedResult ? "Passed" : $"Failed: expected {testResult} got {decryptedResult}");
+		}
+
+		private static BigInteger Encrypt(int m, PublicKey publicKey)
+		{
+			var random = new Random();
+
+			BigInteger b = 0;
+			for(int i = 0; i < publicKey.PK.Length; i++)
+			{
+				if(random.Next(2) == 1)
+					b = b + publicKey.PK[i];
+			}
+			return b + m;
+		}
+
+		private static BigInteger Decrypt(BigInteger c, PrivateKey privateKey, int maxNum)
+		{
+			var m = c % privateKey.P;
+			return m % maxNum;
+		}
+	}
+}
