c4baf16580c5b583f1e9bb031f2fee3a4a95a02d
Maxim Muzafarov <m.muzafarov@gmail.com>
2015-11-11 20:13:07 +0300

    MoL: update logo

--- services/MoL/service/templates.py
+++ services/MoL/service/templates.py
@@ -7,7 +7,7 @@ ABOUT = {
         {'template': """
         <div class="row about">
             <div class="col-sm-4 center-block">
-                <img src="/favicon.png" class="img-responsive">
+                <img src="/mol.png" class="img-responsive">
             </div>
             <div class="col-sm-8">
                 <h1>Ministry of Love</h1>
