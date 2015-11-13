e57ca799c9cff48bc9eab53ab67c17599f9f8ea0
Polina Zonova <polinas@skbkontur.ru>
2015-11-13 23:33:56 +0500

    HM: +status codes and user verification

--- services/HM/server/user.go
+++ services/HM/server/user.go
@@ -13,6 +13,9 @@ func parseUser(r *http.Request) *User {
 	login := r.FormValue("Login")
 	pass := r.FormValue("Pass")
 	
+	if login == "" || pass == "" {
+		return nil
+	}
     result := &User{login, pass}
 	return result
 }
