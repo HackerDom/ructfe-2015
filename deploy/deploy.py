d5992b83af0f87053b9d324bfb04d9569c1df1aa
Bogdan Melnik <teh.ld86@gmail.com>
2015-11-15 16:31:40 +0000

    deploy

--- deploy/deploy.py
+++ deploy/deploy.py
@@ -38,11 +38,16 @@ class Service:
         self.__add_user(team_machine)
         self.__install_run_deps(team_machine)
         self.__copy_files(dirty_machine, team_machine)
+        self.__chmod_user(team_machine)
         self.__post_copy(dirty_machine, team_machine)
 
     def __add_user(self, machine):
         username = self.config[self.name]["username"]
         machine.run('useradd -m {0}'.format(username))
+
+    def __chmod_user(self, machine):
+        username = self.config[self.name]["username"]
+        machine.run('chmod {0}:{0} -R /home/{0}'.format(username))
 
     def __install_build_deps(self, machine):
         if "build_deps" not in self.config[self.name]:
