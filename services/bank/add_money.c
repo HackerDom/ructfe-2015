96a06a737181b6ca922d7e5723c3a80d4d927753
Alexander Bersenev <bay@hackerdom.ru>
2015-11-15 23:20:55 +0500

    bank: simplify cgi code

--- services/bank/add_money.c
+++ services/bank/add_money.c
@@ -6,47 +6,47 @@
 #include "common.h"
 #include "cgi.h"
 
-
-int main() {
-    int ret;
-
-    printf("Content-type: text/html\n\n");
-    printf("<head><title>add_money</title><head>\n");
-
-    char *body = "";
-
-    printf("<body>%s</body>", body);
-
-    char* login = cgigetval("login");
-    char* account = cgigetval("account");
-    char* amount = cgigetval("amount");
-    long amount_long;
-
+int check_args(char* login, char* account, char* amount) {
     if (!login) {
         printf("%s\n", "Error: No login");
-        return 1;
-    }
-
-    if (!login_good(login)) {
-        printf("%s\n", "Error: Bad login");
-        return 1;
+        return -1;
     }
 
     if (!account) {
         printf("%s\n", "Error: No account");
-        return 1;
+        return -1;
+    }
+
+    if (!amount) {
+        printf("%s\n", "Error: No amount");
+        return -1;
+    }
+
+    if (!login_good(login)) {
+        printf("%s\n", "Error: Bad login");
+        return -1;
     }
 
     if (!account_good(account)) {
         printf("%s\n", "Error: Bad account");
         return 1;
     }
+}
 
-    if (!amount) {
-        printf("%s\n", "Error: No amount");
+
+int main() {
+    printf("Content-type: text/html\n\n");
+    printf("<head><title>add_money</title><head>\n");
+
+    char* login = cgigetval("login");
+    char* account = cgigetval("account");
+    char* amount = cgigetval("amount");
+
+    if (check_args(login, account, amount) == -1) {
         return 1;
     }
 
+    unsigned long amount_long;
     amount_long = get_amount(amount);
     if (amount_long == 0) {
         printf("%s\n", "Error: Bad amount");
@@ -54,15 +54,12 @@ int main() {
     }
 
     struct dict t;
-    ret = new_dict(login, &t);
-
-    if(ret == -1) {
+    if(new_dict(login, &t) == -1) {
         printf("Internal error\n");
         return 1;
     }
 
-    long curr_balance = t.get(account);
-    t.set(account, curr_balance + amount_long);
+    t.set(account, t.get(account) + amount_long);
 
     printf("Successful<br>\n");
     printf("<a href='account.cgi?login=%s'>Go Back</a>\n", login);
