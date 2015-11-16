6162c7156ef165781281a994603aeaa97d1235e1
Bogdan Melnik <teh.ld86@gmail.com>
2015-11-16 19:57:12 +0000

    postcopy for electro

--- deploy/electro/postcopy.sh
+++ deploy/electro/postcopy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 echo "deb http://download.mono-project.com/repo/debian wheezy main" > /etc/apt/sources.list.d/mono-xamarin.list
 echo "deb http://download.mono-project.com/repo/debian wheezy-libjpeg62-compat main" >> /etc/apt/sources.list.d/mono-xamarin.list
 
