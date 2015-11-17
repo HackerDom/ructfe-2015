b9c7bfa92c8cf9173365402c3127920f7f39a256
pahaz <pahaz.blinov@gmail.com>
2015-11-18 02:11:33 +0500

    Fix vilnarability

--- services/tax/routes/users.js
+++ services/tax/routes/users.js
@@ -1,6 +1,8 @@
 var router = require('./../router');
 var parse = require('co-body');
 var db = require('./../db');
+var md5 = require('./../utils/hash').md5;
+var seconds = require('./../utils/time').get_seconds;
 
 var f = function *(next) {
   if (!this.user) return this.redirect(router.resolve('login') + '?next=routes.users');
@@ -13,7 +15,13 @@ var f = function *(next) {
     if (!body.name) this.throw(400, 'ERROR: .name required');
     if (!body.private) this.throw(400, 'ERROR: .private data required');
 
-    yield db.pdata.insert({'name': body.name, 'private': body.private, 'user': this.user.name});
+    try {
+      yield db.pdata.insert({'name': body.name, 'private': body.private, 'user': this.user.name,
+                             '_id': md5(this.user.name + seconds())});
+    } catch (err) {
+      return this.throw(400, 'ERROR: ' + err.message);
+    }
+
     this.redirect(router.resolve('index'));
   }
 };
