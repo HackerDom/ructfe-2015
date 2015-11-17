243b57b9992459c122d06103b8d863f1ecdff507
Crassirostris <crassirostris@yandex.com>
2015-11-17 22:01:22 +0500

    Completed checker for hm

--- checkers/hm/hm.checker.py
+++ checkers/hm/hm.checker.py
@@ -14,7 +14,7 @@ __author__ = 'm_messiah, crassirostris'
 OK, GET_ERROR, CORRUPT, FAIL, INTERNAL_ERROR = 101, 102, 103, 104, 110
 
 NAMES_FILENAME = 'names.txt'
-METRICS_FILENAME = 'metrics.txt'
+MAX_PAGE_SIZE = 1024 * 1024
 
 
 def close(code, public="", private=""):
@@ -27,16 +27,20 @@ def close(code, public="", private=""):
 def create_name():
     with open(NAMES_FILENAME, 'r') as f:
         lines = f.readlines()
-    return choice(lines)
+    return choice(lines).strip()
 
 def create_password():
     return sha1(str(time()).encode("utf-8")).hexdigest()
 
 def generate_comment():
-    return "Thy say The Checker!" #TODO: change?
+    return "Totally fine!"
 
 def create_metrics(flag):
-    raise NotImplementedError()
+    return { "Weight": randrange(40, 100),
+        "Blood Pressure": randrange(80, 120),
+        "Pulse": randrange(70, 150),
+        "Walking Distance": randrange(1000, 10 * 1000),
+        "Comment": flag }
 
 class CheckerException(Exception):
     """Custom checker error"""
@@ -51,19 +55,16 @@ class Client(object):
         self.cookie_jar = CookieJar()
 
     def create_user(self):
-        self.open_and_check_ok('/newUser', self.encode_user_data())
+        self.open_and_check_ok('newuser', self.encode_user_data())
 
     def auth(self):
-        self.open_and_check_ok('/login', self.encode_user_data())
+        self.open_and_check_ok('login', self.encode_user_data())
 
     def add_metrics(self, metrics):
-        raise NotImplementedError()
+        self.open_and_check_ok('addhealthmetrics', urlencode(metrics))
 
     def get_metrics(self):
-        raise NotImplementedError()
-
-    def assert_metrics_correctness(self, metrics, flag):
-        raise NotImplementedError()
+        return self.open_and_check_ok('healthmetrics')
 
     def open_and_check_ok(self, path, data = None):
         opener = build_opener(HTTPCookieProcessor(self.cookie_jar))
@@ -74,6 +75,7 @@ class Client(object):
         if response.getcode() != 200:
             raise CheckerException("Recieved status %d on request %s"
                 % (response.getcode(), response.geturl()))
+        return response.read(MAX_PAGE_SIZE).decode('utf-8')
 
     def encode_user_data(self):
         return urlencode({'Login': self.user[0], 'Pass': self.user[1]})
@@ -87,9 +89,9 @@ def check(*args):
         c.create_user()
         c.auth()
         c.add_metrics(create_metrics(flag))
-        metrics = c.get_metrics()
-        c.assert_metrics_correctness(metrics, flag)
-        close(OK)
+        if flag in c.get_metrics():
+            close(OK)
+        raise CheckerException("Didn't find posted flag")
     except http_error as e:
         close(FAIL, "HTTP Error", "HTTP error sending to '%s': %s" % (addr, e))
     except CheckerException as e:
@@ -102,7 +104,8 @@ def put(*args):
     addr = args[0]
     flag_id = args[1]
     flag = args[2]
-    c = Client(addr, (create_name(), create_password()))
+    user = (create_name(), create_password())
+    c = Client(addr, user)
     try:
         c.create_user()
         c.auth()
@@ -123,9 +126,9 @@ def get(*args):
     c = Client(addr, checker_flag_id.split(":"))
     try:
         c.auth()
-        metrics = c.get_metrics()
-        c.assert_metrics_correctness(metrics, flag)
-        close(OK)
+        if flag in c.get_metrics():
+            close(OK)
+        close(GET_ERROR, "Flag is missing")
     except http_error as e:
         close(FAIL, "HTTP Error", "HTTP error sending to '%s': %s" % (addr, e))
     except CheckerException as e:
