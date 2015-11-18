a781ad09e5b6724c0fc2fb5f870e9a5087f7292f
Andrew Gein <andgein@yandex.ru>
2015-11-19 02:26:51 +0500

    Remove some blocks from theme's css and js

--- static/static/js/theme.js
+++ static/static/js/theme.js
@@ -9,64 +9,6 @@ $(window).load(function() {
 });
 
 
-
-
-
-/////////////////////////////////////////////////////////////////////
-// jQuery for page scrolling feature - requires jQuery Easing plugin
-/////////////////////////////////////////////////////////////////////
-
-$('.page-scroll').bind('click', function(event) {
-    var $anchor = $(this);
-    $('html, body').stop().animate({
-        scrollTop: $($anchor.attr('href')).offset().top -64
-    }, 1500, 'easeInOutExpo');
-    event.preventDefault();
-});
-
-
-
-////////////////////////////////////////////////////////////////////////
-// On-Scroll Animated Header: https://github.com/codrops/AnimatedHeader
-////////////////////////////////////////////////////////////////////////
-
-var cbpAnimatedHeader = (function() {
-
-    var docElem = document.documentElement,
-        header = document.querySelector( '.navbar-fixed-top' ),
-        didScroll = false,
-        changeHeaderOn = 10;
-
-    function init() {
-        window.addEventListener( 'scroll', function( event ) {
-            if( !didScroll ) {
-                didScroll = true;
-                setTimeout( scrollPage, 250 );
-            }
-        }, false );
-    }
-
-    function scrollPage() {
-        var sy = scrollY();
-        if ( sy >= changeHeaderOn ) {
-            classie.add( header, 'navbar-shrink' );
-        }
-        else {
-            classie.remove( header, 'navbar-shrink' );
-        }
-        didScroll = false;
-    }
-
-    function scrollY() {
-        return window.pageYOffset || docElem.scrollTop;
-    }
-
-    init();
-
-})();
-
-
-
 //////////////////////////////////////////////
 // Highlight the top nav as scrolling occurs
 //////////////////////////////////////////////
@@ -75,7 +17,6 @@ $('body').scrollspy({
     target: '.navbar',
     offset: 65
 })
-
 
 
 ////////////////////////////////////////////////////
