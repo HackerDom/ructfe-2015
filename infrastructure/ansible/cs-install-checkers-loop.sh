5c743083e43e8c40c01c93ecbced4b63cfdfd2dc
dimmoborgir <dimmoborgir@gmail.com>
2015-11-19 00:58:17 +0500

    auto-update checkers

--- infrastructure/ansible/cs-install-checkers-loop.sh
+++ infrastructure/ansible/cs-install-checkers-loop.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 while [ 1 ];
 do
+    echo " *** UPDATING CHECKERS ONLY *** "
     echo " *** Started at: "`date`
-    ./cs-install.sh
+    ./cs-install.sh -t checkers
     echo " *** Finished at: "`date`
     sleep 180
 done
