59badb1269c829ba62c36201ef93334b263b7daa
dimmoborgir <dimmoborgir@gmail.com>
2015-11-20 02:18:05 +0500

    added prod vpn teams to test config

--- infrastructure/ansible/cs-install/roles/checksystem/templates/gen-dev.sh
+++ infrastructure/ansible/cs-install/roles/checksystem/templates/gen-dev.sh
@@ -10,3 +10,8 @@ do
     echo "    {name => 'g7 team$i', network => '10.70.1.$i/32', host => 'devnew$i.e.ructf.org'},"
 done
 
+for i in `seq 500 510`
+do
+    IP="10.$((60+$i/256)).$(($i%256)).0/24"
+    echo "    {name => 'ProdVPN team$i', network => '$IP', host => 'team$i.e.ructf.org'},"
+done
