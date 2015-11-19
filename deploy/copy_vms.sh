a124ec060ff63d940069056ad7576db5098c47dd
Bogdan Melnik <teh.ld86@gmail.com>
2015-11-19 16:55:47 +0000

    limits and restart on-failure

--- deploy/copy_vms.sh
+++ deploy/copy_vms.sh
@@ -5,7 +5,7 @@ VBoxManage unregistervm team$2-x64 --delete
 VBoxManage clonevm team$1-x64 --mode all --name team$2-x64 --register
 VBoxManage startvm team$2-x64 --type headless
 
-ping -c 10 10.70.0.$1
+ping -c 25 10.70.0.$1
 
 ssh -i /tmp/deploy-key-vbox root@10.70.0.$1 sed -i "s/$1/$2/" /etc/network/interfaces
 ssh -i /tmp/deploy-key-vbox root@10.70.0.$1 /etc/init.d/networking restart &
